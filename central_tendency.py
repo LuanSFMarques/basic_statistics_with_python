890df8062c10ecb067461db44378edd9f40f24af
Luan <luansfmarques@gmail.com>
2025-03-20 17:46:25 -0300

    CREATE central_tendency.py

---
+++ central_tendency.py
@@ -0,0 +1,8 @@
+import numpy as np
+from scipy import stats
+
+array1 = np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+print(f"Numpy Array 1: {array1}\n")
+
+array2 = np.arange(0, 101, 10)
+print(f"Numpy Array 2: {array2}\n")
